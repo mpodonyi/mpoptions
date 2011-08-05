c9d7cbffb2bd80bcf3ded99d4a80be1a57f29060
Mike Podonyi <mikepodonyi@gmail.com>
2011-08-05 17:48:26 +0100

    add general flow extension

---
+++ src/MPOptions/Extensions/GeneralFlowExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPOptions.Extensions
+{
+    public static class GeneralFlowExtensions
+    {
+        //public static T Add<T>(this IGeneralFlow<T> element, IEnumerable<Option> object[] objs)
+        //{
+        //    return element.Add(from i in objs
+        //                       where i is Command
+        //}
+
+        public static T Add<T>(this IGeneralFlow<T> element, params object[] objs) where T:Command
+        {
+            return (T)((T)element).Add(
+                                (from i in objs
+                                 where i is Command
+                                 select i as Command).ToArray()
+                               )
+                               .Add(
+                               (from i in objs
+                                where i is Option
+                                select i as Option).ToArray()
+                               )
+                               .Add
+                               (
+                               (from i in objs
+                                where i is Argument
+                                select i as Argument).SingleOrDefault()
+                               );
+
+        }
+
+        //public static Command Add(this IGeneralFlow<Command> element, params object[] objs)
+        //{
+        //    return element.Add(
+        //                        (from i in objs
+        //                         where i is Command
+        //                         select i as Command).ToArray()
+        //                       )
+        //                       .Add(
+        //                       (from i in objs
+        //                        where i is Option
+        //                        select i as Option).ToArray()
+        //                       )
+        //                       .Add
+        //                       (
+        //                       (from i in objs
+        //                        where i is Argument
+        //                        select i as Argument).SingleOrDefault()
+        //                       );
+
+        //}
+
+        //public static RootCommand Add(this IGeneralFlow<RootCommand> element, params object[] objs)
+        //{
+        //    return element.Add(
+        //                        (from i in objs
+        //                         where i is Command
+        //                         select i as Command).ToArray())
+        //                       .Add(
+        //                       (from i in objs
+        //                        where i is Option
+        //                        select i as Option).ToArray())
+        //                       .Add
+        //                       ((from i in objs
+        //                         where i is Argument
+        //                         select i as Argument).SingleOrDefault());
+
+        //}
+    }
+}
